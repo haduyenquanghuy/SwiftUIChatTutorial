1fdd377dfeae685c5b4e38d83667d43d3037d54b
huyhdq <huyhdq@ftech.ai>
2023-06-14 18:06:44 +0700

    Initial Commit

---
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  SwiftUIChatTutorial
+//
+//  Created by Ha Duyen Quang Huy on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
